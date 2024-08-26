5de603218c17177cb7b43a2365819b48e5daf7c8
Gabriel Vazquez <gabrielv@sonatype.com>
2024-08-26 17:04:52 -0300

    using logger

--- src/main/java/MyApp.java
+++ src/main/java/MyApp.java
@@ -1,5 +1,10 @@
+import java.util.logging.Logger;
+
 public class MyApp {
+
+    static Logger logger = Logger.getLogger(MyApp.class.getName());
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        logger.info("Hello, World!");
     }
 }
